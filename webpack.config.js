05654a11efe3706a9aeff927fb73a5a601d17559
d0rian <dorian.beauchesne@free.fr>
2020-03-28 16:25:14 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.ts',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }, ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'app.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
